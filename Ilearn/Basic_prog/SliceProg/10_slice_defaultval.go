60232a93903154a34b9f3ecbef6e1b766ad331c8
rajashri570 <rajashri26chhatrikar@gmail.com>
2024-01-10 10:13:54 +0530

    added slice

--- Ilearn/Basic_prog/SliceProg/10_slice_defaultval.go
+++ Ilearn/Basic_prog/SliceProg/10_slice_defaultval.go
@@ -2,17 +2,23 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
 	// create a slice variable
 
-	var slice = new([20]int)[0:10]
+	var slice = new([20]int)
+	var arr [5]int = [5]int{2, 34, 5, 67, 8}
 	fmt.Println("Length :", len(slice))
 	fmt.Println("Capacity :", cap(slice))
+	fmt.Println(reflect.ValueOf(arr).Kind())
+	//fmt.Printf("%T", slice)
 
-	fmt.Println(slice)
+	fmt.Println(slice, arr)
 }
 
 /* rdolzake@rdolzake-HP-245-G5-Notebook-PC:~/GOLANG/Go-lang/Ilearn/Basic_prog/Queries/SliceProg$ go run 10_slice_defaultval.go
